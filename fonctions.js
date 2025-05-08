4eb6074517bd54b39b3725188fe1a20cecf8ec6b
BambaLeDevelloper <bambasyll22@gmail.com>
2025-05-08 22:10:00 +0000

    j essai de pratiquer le cours

---
+++ fonctions.js
@@ -0,0 +1,19 @@
+function estClique(id) {
+    var button = document.getElementById(id);
+    var prenom = document.getElementById('Prenom');
+    var nom = document.getElementById('Nom');
+    var tel = document.getElementById('tel');
+    var email = document.getElementById('email');
+    button.addEventListener("click", function(event) {
+      event.preventDefault();  
+      if (prenom.value.trim() === "" || nom.value.trim()==="" || tel.value.trim() ==="" || email.value.trim()==="") {
+        alert("Veuillez remplir tous les champs !!!");
+      } else {
+        document.writeln("tous les champs sont remplis ");
+        return true;
+      } 
+    });
+  }
+  estClique('ok');
+  
+  
